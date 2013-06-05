9372d6eaf6f0e012358e523ee97b1a3eaf550ed5
Andre Baltieri <andrebaltieri@hotmail.com>
2013-06-05 16:55:33 -0300

    Versao final do site

---
+++ src/MvcSummit2013/MvcSummit2013/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSummit2013.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Grade()
+        {
+            return View();
+        }
+
+        public ActionResult Palestrantes()
+        {
+            return View();
+        }
+
+        public ActionResult Contato()
+        {
+            return View();
+        }
+    }
+}
